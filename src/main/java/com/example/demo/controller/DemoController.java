b2fdc0c819aec8d5a8b1fec1e609d3df01c65ee6
kawano <kyosuke.1215@icloud.com>
2024-06-21 13:57:43 +0900

    test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -81,7 +81,7 @@ public class DemoController {
 	@GetMapping("/test2")
 	public Test check() {
 		Test test = new Test();
-		test.setName("山田");
+		test.setName("大谷翔平");
 		test.setScore(100);
 		
 		return test;
